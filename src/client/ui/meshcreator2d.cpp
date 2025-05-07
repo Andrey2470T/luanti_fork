305743755e5c27277ed7d39b71e1152432a1e0a8
Andrey2470T <andreyt2203@gmail.com>
2025-05-07 13:27:05 +0300

    MeshCreator2D: functions for creating image rectangle meshes

--- src/client/ui/meshcreator2d.cpp
+++ src/client/ui/meshcreator2d.cpp
@@ -3,7 +3,7 @@
 MeshBuffer *MeshCreator2D::createLine(
     const v2f &startPos, const v2f &endPos, const img::color8 &color)
 {
-    MeshBuffer *buf = new MeshBuffer(MeshBufferType::VERTEX_INDEX, CurVT);
+    MeshBuffer *buf = new MeshBuffer(MeshBufferType::VERTEX, CurVT);
 
     appendLine(buf, startPos, endPos, color);
 
@@ -16,9 +16,10 @@ MeshBuffer *MeshCreator2D::createLine(const line2f &line, const img::color8 &col
 }
 
 MeshBuffer *MeshCreator2D::createTriangle(
-    const v2f &pos1, const v2f &pos2, const v2f &pos3, const img::color8 &color)
+    const v2f &pos1, const v2f &pos2, const v2f &pos3, const img::color8 &color,
+    const v2f &uv1, const v2f &uv2, const v2f &uv3)
 {
-    MeshBuffer *buf = new MeshBuffer(MeshBufferType::VERTEX_INDEX, CurVT);
+    MeshBuffer *buf = new MeshBuffer(MeshBufferType::VERTEX, CurVT);
 
     appendTriangle(buf, pos1, pos2, pos3, color);
 
@@ -26,19 +27,46 @@ MeshBuffer *MeshCreator2D::createTriangle(
 }
 
 MeshBuffer *MeshCreator2D::createRectangle(
-    const rectf &rect, const std::array<img::color8, 4> &colors)
+    const rectf &rect, const std::array<img::color8, 4> &colors,
+    const std::array<v2f, 4> &uvs)
 {
     MeshBuffer *buf = new MeshBuffer(MeshBufferType::VERTEX_INDEX, CurVT);
 
-    appendRectangle(buf, rect, colors);
+    appendRectangle(buf, rect, colors, uvs);
 
     return buf;
 }
 
-MeshBuffer *MeshCreator2D::createRectangle()
+MeshBuffer *MeshCreator2D::createUnitRectangle()
 {
     return createRectangle(
-        rectf(v2f(-0.5f, 0.5f), v2f(0.5f, -0.5f)), {});
+        rectf(v2f(-1.0f, 1.0f), v2f(1.0f, -1.0f)), {});
+}
+
+MeshBuffer *MeshCreator2D::createImageRectangle(
+    const v2u &imgSize, const rectf &srcRect, const rectf &destRect,
+    const std::array<img::color8, 4> &colors, bool flip)
+{
+    f32 invW = 1.0f / imgSize.X;
+    f32 invH = 1.0f / imgSize.Y;
+
+    f32 topY = flip ? srcRect.LRC.Y : srcRect.ULC.Y;
+    f32 bottomY = flip ? srcRect.ULC.Y : srcRect.LRC.Y;
+
+    std::array<v2f, 4> tcoords{
+        v2f(srcRect.ULC.X * invW, topY * invH),
+        v2f(srcRect.LRC.X * invW, topY * invH),
+        v2f(srcRect.LRC.X * invW, bottomY * invH),
+        v2f(srcRect.ULC.X * invW, bottomY * invH)
+    };
+
+    return createRectangle(destRect, colors, tcoords);
+}
+
+MeshBuffer *MeshCreator2D::createImageUnitRectangle(bool flip)
+{
+    return createImageRectangle(v2u(1), rectf(v2f(1.0f)),
+        rectf(v2f(-1.0f, 1.0f), v2f(1.0f, -1.0f)), {img::color8(), img::color8(), img::color8(), img::color8()}, flip);
 }
 
 void MeshCreator2D::appendVertex(MeshBuffer *buf, const v2f &pos, const img::color8 &color, const v2f &uv)
@@ -52,21 +80,23 @@ void MeshCreator2D::appendLine(MeshBuffer *buf, const v2f &startPos, const v2f &
     appendVertex(buf, endPos, color);
 }
 
-void MeshCreator2D::appendTriangle(MeshBuffer *buf, const v2f &pos1, const v2f &pos2, const v2f &pos3, const img::color8 &color)
+void MeshCreator2D::appendTriangle(MeshBuffer *buf, const v2f &pos1, const v2f &pos2, const v2f &pos3, const img::color8 &color,
+    const v2f &uv1, const v2f &uv2, const v2f &uv3)
 {
-    appendVertex(buf, pos1, color, v2f(0.0f, 0.0f));
-    appendVertex(buf, pos2, color, v2f(1.0f, 0.0f));
-    appendVertex(buf, pos3, color, v2f(0.5f, 1.0f));
+    appendVertex(buf, pos1, color, uv1);
+    appendVertex(buf, pos2, color, uv2);
+    appendVertex(buf, pos3, color, uv3);
 }
 
-void MeshCreator2D::appendRectangle(MeshBuffer *buf, const rectf &rect, const std::array<img::color8, 4> &colors)
+void MeshCreator2D::appendRectangle(MeshBuffer *buf, const rectf &rect, const std::array<img::color8, 4> &colors,
+    const std::array<v2f, 4> &uvs)
 {
-    std::array<u32, 6> indices = {0, 1, 2, 2, 1, 3};
+    std::array<u32, 6> indices = {0, 1, 2, 2, 3, 0};
 
-    appendVertex(buf, rect.ULC, colors[0], v2f(0.0f, 0.0f));
-    appendVertex(buf, v2f(rect.ULC.X, rect.LRC.Y), colors[1], v2f(1.0f, 0.0f));
-    appendVertex(buf, rect.LRC, colors[2], v2f(0.0f, 1.0f));
-    appendVertex(buf, v2f(rect.LRC.X, rect.ULC.Y), colors[3], v2f(1.0f, 1.0f));
+    appendVertex(buf, rect.ULC, colors[0], uvs[0]);
+    appendVertex(buf, v2f(rect.LRC.X, rect.ULC.Y), colors[1], uvs[1]);
+    appendVertex(buf, rect.LRC, colors[2], uvs[2]);
+    appendVertex(buf, v2f(rect.ULC.X, rect.LRC.Y), colors[3], uvs[3]);
 
     for (u32 i = 0; i < 6; i++)
         buf->setIndexAt(indices[i]);
--- src/client/ui/meshcreator2d.h
+++ src/client/ui/meshcreator2d.h
@@ -30,17 +30,28 @@ public:
 
     // pos1, pos2, pos3 must be ordered counter-clockwise!
     MeshBuffer *createTriangle(
-        const v2f &pos1, const v2f &pos2, const v2f &pos3, const img::color8 &color=img::color8());
+        const v2f &pos1, const v2f &pos2, const v2f &pos3, const img::color8 &color=img::color8(),
+        const v2f &uv1=v2f(0.0f, 0.0f), const v2f &uv2=v2f(0.5f, 1.0f), const v2f &uv3=v2f(1.0f, 0.0f));
 
+    // vertices are filled in the clockwise order!
     MeshBuffer *createRectangle(
-        const rectf &rect, const std::array<img::color8, 4> &colors);
+        const rectf &rect, const std::array<img::color8, 4> &colors,
+        const std::array<v2f, 4> &uvs={v2f(0.0f,1.0f),v2f(1.0f,1.0f),v2f(1.0f,0.0f),v2f(0.0f,0.0f)});
 
-    MeshBuffer *createRectangle();
+    MeshBuffer *createUnitRectangle();
+
+    MeshBuffer *createImageRectangle(
+        const v2u &imgSize, const rectf &srcRect, const rectf &destRect,
+        const std::array<img::color8, 4> &colors, bool flip);
+
+    MeshBuffer *createImageUnitRectangle(bool flip);
 private:
     void appendVertex(MeshBuffer *buf, const v2f &pos, const img::color8 &color=img::color8(), const v2f &uv=v2f());
     void appendLine(MeshBuffer *buf, const v2f &startPos, const v2f &endPos, const img::color8 &color=img::color8());
     // pos1, pos2, pos3 must be ordered counter-clockwise!
-    void appendTriangle(MeshBuffer *buf, const v2f &pos1, const v2f &pos2, const v2f &pos3, const img::color8 &color=img::color8());
-    void appendRectangle(MeshBuffer *buf, const rectf &rect, const std::array<img::color8, 4> &colors);
+    void appendTriangle(MeshBuffer *buf, const v2f &pos1, const v2f &pos2, const v2f &pos3, const img::color8 &color=img::color8(),
+        const v2f &uv1=v2f(0.0f, 0.0f), const v2f &uv2=v2f(0.5f, 1.0f), const v2f &uv3=v2f(1.0f, 0.0f));
+    void appendRectangle(MeshBuffer *buf, const rectf &rect, const std::array<img::color8, 4> &colors,
+        const std::array<v2f, 4> &uvs={v2f(0.0f,1.0f),v2f(1.0f,1.0f),v2f(1.0f,0.0f),v2f(0.0f,0.0f)});
 
 };
