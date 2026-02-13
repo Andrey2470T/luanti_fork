d90c50193f895969aeafda0618ffec2f9332e185
Andrey2470T <andreyt2203@gmail.com>
2026-02-13 18:55:42 +0300

    Returned .gitmodules, added "with: submodules: 'recursive'" in workflows/build.yml

--- util/ci/common.sh
+++ util/ci/common.sh
@@ -41,13 +41,3 @@ install_macos_deps() {
 	brew unlink $(brew ls --formula)
 	brew link "${pkgs[@]}"
 }
-
-add_submodules() {
-	cd ./lib
-
-	sudo git submodule add -b v5.3.0 https://github.com/assimp/assimp.git
-	sudo git submodule add https://github.com/Andrey2470T/irrlicht_redo.git
-	sudo git submodule add https://github.com/TeamHypersomnia/rectpack2D.git
-	
-	cd ..
-}
