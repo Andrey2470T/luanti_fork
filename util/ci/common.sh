610bed5889df9f0fe8d91dd79ec2e4ad311691f3
Andrey2470T <andreyt2203@gmail.com>
2026-02-15 00:57:33 +0300

    Separated the workflows into different yml files

--- util/ci/common.sh
+++ util/ci/common.sh
@@ -9,14 +9,6 @@ install_linux_deps() {
 		libopenal-dev libpq-dev libleveldb-dev libcurl4-openssl-dev libzstd-dev
 	)
 
-	if [[ "$1" == "--no-irr" ]]; then
-		shift
-	else
-		local ver=$(cat misc/irrlichtmt_tag.txt)
-		wget "https://github.com/minetest/irrlicht/releases/download/$ver/ubuntu-bionic.tar.gz"
-		sudo tar -xaf ubuntu-bionic.tar.gz -C /usr/local
-	fi
-
 	sudo apt-get update
 	sudo apt-get install -y --no-install-recommends "${pkgs[@]}" "$@"
 
@@ -27,11 +19,26 @@ install_linux_deps() {
 	"
 }
 
+# Linux -> win32 cross-compiling only
+install_mingw_deps() {
+	local bits=$1
+	local nsis=$2
+
+	local pkgs=(gettext wine wine$bits)
+	[[ "$nsis" == "true" ]] && pkgs+=(nsis ccache)
+
+	sudo dpkg --add-architecture i386
+	sudo apt-get update
+	sudo apt-get install -y --no-install-recommends "${pkgs[@]}"
+}
+
 # macOS build only
-install_macos_deps() {
+install_macos_brew_deps() {
+	# Uninstall the bundled cmake, it is outdated, and brew does not want to install the newest version with this one present since they are from different taps.
+	brew uninstall cmake || :
 	local pkgs=(
-		cmake gettext sdl2 freetype gmp jpeg-turbo jsoncpp leveldb
-		libogg libpng libvorbis luajit zstd
+		cmake gettext freetype gmp jpeg-turbo jsoncpp leveldb
+		libogg libpng libvorbis luajit zstd sdl2
 	)
 	export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 	export HOMEBREW_NO_INSTALL_CLEANUP=1
@@ -40,4 +47,25 @@ install_macos_deps() {
 	brew install --display-times "${pkgs[@]}"
 	brew unlink $(brew ls --formula)
 	brew link "${pkgs[@]}"
+}
+
+install_macos_precompiled_deps() {
+	local osver=$1
+	local arch=$2
+
+	local pkgs=(
+		cmake wget
+	)
+	export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+	export HOMEBREW_NO_INSTALL_CLEANUP=1
+	# contrary to how it may look --auto-update makes brew do *less*
+	brew update --auto-update
+	brew install --display-times "${pkgs[@]}"
+	brew unlink $(brew ls --formula)
+	brew link "${pkgs[@]}"
+
+	cd /Users/Shared
+	wget -O macos${osver}_${arch}_deps.tar.gz https://github.com/luanti-org/luanti_macos_deps/releases/download/latest/macos${osver}_${arch}_deps.tar.gz
+	tar -xf macos${osver}_${arch}_deps.tar.gz
+	cd ~
 }
